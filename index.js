85c103abba67374ee70e9add0d223aee284b07a6
PavelPoroskov <pavl1853@gmail.com>
2019-03-02 01:43:23 +0300

    test use k8s\secret

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ app.get('/bar', (req, res) => {
   res.status(200).json('welcome to bar');
 });
 
+app.get('/my_ig', (req, res) => {
+  needle
+    .get('https://api.instagram.com/v1/users/self/?access_token=' + process.env.IG_ACCESS_TOKEN,
+    (err, response, data) => {
+      res.status(200).json(data);
+    })
+});
+
 app.listen(port, () => console.log('Magic happens on port', port));
 
 module.exports.app = app;
