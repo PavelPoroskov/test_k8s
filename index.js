508e52a3262bae8b1e84e1291459ee931e2b86d5
PavelPoroskov <pavl1853@gmail.com>
2019-03-02 01:57:42 +0300

    off use k8s\secret

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('express')();
-const needle = require('needle');
+//const needle = require('needle');
 
 const port = process.env.PORT || 8000;
 
@@ -15,13 +15,13 @@ app.get('/bar', (req, res) => {
   res.status(200).json('welcome to bar');
 });
 
-app.get('/my_ig', (req, res) => {
-  needle
-    .get('https://api.instagram.com/v1/users/self/?access_token=' + process.env.IG_ACCESS_TOKEN,
-    (err, response, data) => {
-      res.status(200).json(data);
-    })
-});
+// app.get('/my_ig', (req, res) => {
+//   needle
+//     .get('https://api.instagram.com/v1/users/self/?access_token=' + process.env.IG_ACCESS_TOKEN,
+//     (err, response, data) => {
+//       res.status(200).json(data);
+//     })
+// });
 
 app.listen(port, () => console.log('Magic happens on port', port));
 
